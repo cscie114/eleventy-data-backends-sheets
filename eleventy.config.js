1d7ec290529aec1ac284f8303f54218b7e020901
David Heitmeyer <david@heitmeyer.org>
2024-03-07 12:23:05 -0500

    spring 2024

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,11 @@ const inspect = require("util").inspect;
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/assets/**");
 	eleventyConfig.addFilter("inspect", function(content) {  return `<pre>${inspect(content)}</pre>` });
-
+	eleventyConfig.addShortcode("hostname", function (url) {
+		let urlObj = new URL(url);
+		let hostname = urlObj.hostname;
+		return hostname;
+	  });
 	return {
     	"dir" : {
         	"input": "src",
